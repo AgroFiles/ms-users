3443873e9a8251ca03abd514c0f3a222f3d83451
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2025-09-20 15:57:45 -0300

    config

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,33 @@ plugins {
     alias(libs.plugins.ktor)
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
 group = "com.example"
 version = "0.0.1"
 
 application {
-    mainClass = "io.ktor.server.netty.EngineMain"
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+tasks {
+    shadowJar {
+        archiveBaseName.set("app")
+        archiveClassifier.set("") // evita el "-all"
+        archiveVersion.set("")    // evita el "-1.0.0"
+    }
 }
 
 dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
-    implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
+
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
