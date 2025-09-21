a47ceb7e9f72033a86f21abe695022589911a6a2
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2025-09-20 21:48:07 -0300

    gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,38 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
+    kotlin("plugin.serialization") version libs.versions.kotlin.get()
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 group = "com.example"
 version = "0.0.1"
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+    mainClass.set("com.example.ApplicationKt")
 }
 
 tasks {
     shadowJar {
         archiveBaseName.set("app")
-        archiveClassifier.set("") // evita el "-all"
-        archiveVersion.set("")    // evita el "-1.0.0"
+        archiveClassifier.set("")
+        archiveVersion.set("")
     }
 }
 
@@ -29,6 +41,15 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.java.time)
+    implementation(libs.postgresql.driver)
+    implementation("io.github.cdimascio:java-dotenv:5.2.2")
 
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
